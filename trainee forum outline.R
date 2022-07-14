be78a35c270a4a4366c418385389b57d0b024c01
Romit <romit@mbpro_i7>
2022-07-14 09:55:03 +0100

    notes

---
+++ trainee forum outline.R
@@ -0,0 +1,130 @@
+# What is data science?
+
+#       Measurement / Data collation / curation / cleaning / sharing
+#       Statistics / inference / prediction (ML/AI) 
+#       Visualisation / Dissemination
+#       Can be applied to all natural and life sciences
+
+#   
+# The old model: 
+
+#  Research
+#  Generate a hypothesis
+#  Devise a (controlled) experiment 
+#  Measure / collect data
+#  Data analysis and interpretation
+#  Rinse and repeat
+#  Publication  
+#  
+#   
+#  So what has changed?
+#  Data availability
+#      Routinely collected and stored
+#      Automated
+#      Easier to generate due to elentronic healthcare records
+#      More people generating data (Reseach / QI)
+#      Public databases with healthcare / socioeconomic / biological
+#
+#  Accesibility
+#     Not just the data but the METHODS and TOOLS
+#     Recent revolution in how programme languages are designed
+#     Programming languages no longer the domain of statisticians and 
+#       software engineers 
+#     Domain experts (like you) can use these tools on their home computers
+#       even where data is large you can use cloud computing methods 
+#     Trusted research environements (TREs)
+#         Sanboxed environments where priviedlged (potentially identifiable) data 
+#         can be accessed and queries run to address specific problems or questions
+#          Data cannot leave without approval
+
+#   The need for a narrative
+#       Data no longer just presented to super-specialist audiences 
+#          (conferences and journals) but to colleagues (clinical and 
+#         non-clinical) and beyond
+#       The narrative can be used to
+#           Disseminate new knowledge
+#           Highlight problems
+#           Change behaviour
+#           Change policy
+#                       
+#     Data science offers automated workflows
+#       that generate: stats / presentations / report / blog posts / dashboards
+#           thesis
+#
+
+#     Yeah but I know / like / am comfortable with MS Excel
+#         Excel maxes out after (18k, 2000)  1 million rows (Office 365)
+#         Its great for small projects using single tables
+#         The real power of excel is in its macros and more advanced commands
+#            -> computer grinds to a halt
+#         No reproducibility
+#           You can't track your clicks and the steps you made to clean / 
+#           organise your data from raw -> usable
+#         New data needs to undergo the same process 
+#         These steps are best done in computer program
+#         Easier to address edge-cases consistently
+#         Transparent methodolgy allows others to interogate and correct methods
+#         Iteration improves the efficiency over time 
+#     
+
+#     Back to business: YEAH BUT I JUST WANT TO GIVE AN ANAESTHETIC!
+#         Data changes behaviour and practice
+#         NAP-4 
+#         NELA
+#         National hip fracture databases (spinal vs GA)
+#           Health services research
+#         Pulse oximetry values in non-white individuals (MIMIC and eICU)
+#
+
+#     Data science allows us to ask questions that would only otherwise be 
+#     available to specialised researchers
+#       COVID H and L phenotypes
+#       Intra-operative opiates
+#       Tracking COVID genotypes locally
+
+#     Gives you the tools to question / support the assertions of others
+#       (especially if they are an opinion / perspective that relies on their
+#         eminence)
+
+#     It's no longer good enough to say 
+#         "lets try this, it's unlikely to do any harm"
+#         LOVIT trial
+
+#     Where do I go next?
+
+#       Find a question that interests you
+#       Learn a language
+#       Is this data already available? (locally, public repository)
+#       Work out how to clean and organise the data
+#       Come and have a chat
+#       Much quicker than a biological experiment based study
+#       Probably more relevant to clinical care
+
+#   How deep does this rabbit hole go?
+
+#       As far as you like
+#           Cambrdige center for AI in healthcare
+#           Big Data insttitute, University of Oxford
+#           UCL Institute for Health Informatics
+#           HDR UK
+
+#   What do I get out of it?
+#         
+#           Problem solving
+#           Using a different part of my brain that doesn't involve
+#               appeasing a bit of paperwork / protocol / 
+#                    broken NHS software that was never fit for purpose
+#           Genuine "Eureka!" moments   
+#           Collaboration
+#           High turnover of questions
+#           Contributing to some importanta questions in my field
+#           Less exhausted by clinical workload
+#           Think about how to make changes - what bit of data 
+#               do I need to provide the narrative that will support changes?
+
+#     So in summary
+#       Data is NOT the new gold
+#       But it is now more accessible than experimental research (which it does not replace)
+#       It is not a life sentance
+#       Explore for yourself how far you can get down the rabbit-hole
+
